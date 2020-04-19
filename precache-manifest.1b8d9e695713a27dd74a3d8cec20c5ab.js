60378eac5fc6c7e83cfc22fca2a304c57a8b2304
jtbspace <jtbspace.tech@gmail.comm>
2020-04-19 15:11:40 +1200

    Updates

---
+++ precache-manifest.1b8d9e695713a27dd74a3d8cec20c5ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "211e5d2c4435667eab6f119602f692b1",
+    "url": "/eyetube/index.html"
+  },
+  {
+    "revision": "ceaa336cfae78ee5bfc5",
+    "url": "/eyetube/static/css/main.2949e209.chunk.css"
+  },
+  {
+    "revision": "433e201538637a670de5",
+    "url": "/eyetube/static/js/2.19481fb6.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/eyetube/static/js/2.19481fb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ceaa336cfae78ee5bfc5",
+    "url": "/eyetube/static/js/main.66804718.chunk.js"
+  },
+  {
+    "revision": "445172ba2b96dd33ec7a",
+    "url": "/eyetube/static/js/runtime-main.95bbcc16.js"
+  }
+]);
